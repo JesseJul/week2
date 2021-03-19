eca3d4da6f65b446b959339e96dac1ff8b9265ea
JayTheMate <julenius.jesse@gmail.com>
2021-03-19 16:35:20 +0200

    Working as it should

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const catRouter = require('./routes/catRoute');
+
+app.use(express.static('public'));
+//app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use('/cat', catRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
